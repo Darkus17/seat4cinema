b7858083a1594c32cd3571e326d5cc4154d14f9f
Darkus17 <tulaganovaleisan@gmail.com>
2025-11-11 21:28:48 +0500

    Update settings.py

--- seat4cinema/cinema_project/settings.py
+++ seat4cinema/cinema_project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -140,5 +141,6 @@ LOGOUT_REDIRECT_URL = '/'
 
 
 LOGIN_URL = '/accounts/login/'
+
 
 
